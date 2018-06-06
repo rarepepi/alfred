7f5bd7ef7a3b71854b7fa5c2b53e89cea28bc355
Pepi <pepimartinez@protonmail.com>
2018-06-05 23:22:01 -0400

    First commit

---
+++ bot.py
@@ -0,0 +1,15 @@
+import config
+from binance.client import Client
+client = Client(config.APIKEY, config.APISECRET)
+
+
+def get_ticker():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
