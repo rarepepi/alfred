80765a824065a9e476904e2d2641bac3e0980b9b
Pepi <pepimartinez@protonmail.com>
2018-06-10 11:34:44 -0400

    pip file

--- alfred/alfred.py
+++ alfred/alfred.py
@@ -1,11 +1,14 @@
-import config
+from config import CONFIG
 from binance.client import Client
-client = Client(config.APIKEY, config.APISECRET)
+client = Client(CONFIG['APIKEY'], CONFIG['APISECRET'])
 
 
 def main():
     info = client.get_account()
-    print(info)
+    assets = info['balances']
+    for asset in assets:
+        print(asset['asset'])
+        print(asset['free'])
 
 
 if __name__ == '__main__':
