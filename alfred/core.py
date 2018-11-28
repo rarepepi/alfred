ce758ab0b508b9435c6d7ed597e65c9445171d1d
Pepi <pepimartinez@protonmail.com>
2018-11-28 11:57:58 -0500

    Gemini balance

--- alfred/core.py
+++ alfred/core.py
@@ -1,93 +1,21 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
-import json
-import requests
-from bs4 import BeautifulSoup
-from binance.client import Client
+import config
+import gemini
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
-# Load config
-with open('config.json') as file:
-        config = json.load(file)
-
-binance = Client(config['binance']['apikey'], config['binance']['apisecret'])
-
-
-# Genesis grades
-def genesis_get_grades():
-
-    # Create session
-    session_requests = requests.session()
-
-    # Get login page
-    result = session_requests.get(config['genesis']['login_url'])
-
-    # Create payload
-    payload = {
-        "j_username": config['genesis']['username'],
-        "j_password": config['genesis']['password'],
-    }
-
-    # Perform login
-    result = session_requests.post(config['genesis']['login_url'], data=payload, headers=dict(referer=config['genesis']['login_url']))
-
-    # Scrape url
-    result = session_requests.get(config['genesis']['grade_url'], headers=dict(referer=config['genesis']['grade_url']))
-    soup = BeautifulSoup(result.content, 'lxml')
-
-    # Get rows of classes
-    courses = soup.find_all('tr', 'listrowodd') + \
-        soup.find_all('tr', 'listroweven')
-
-    classes = []
-    grades = []
-    percents = []
-
-    # Add soups into lists
-    for x in range(0, len(courses)):
-        if "No Grades" not in courses[x].find('td', {'class':'cellCenter'}).text:
-            classes.append(courses[x].find('span', {'class':'categorytab'}).text.strip())
-            grades.append(courses[x].find('td', {'width':'70%'}).text.strip())
-            percents.append(courses[x].find('td', {'width': '30%'}).text.strip())
-
-    final_grades = ""
-    for x in range(0, len(classes)):
-        final_grades += classes[x] + " | " + grades[x] + " | " + percents[x] + "\n------------------------------------\n"
-
-    return(final_grades)
-
-
-# Binance info
-def get_btc_value(ticker, amount):
-    pass
-
-
-def binance_get_balance():
-    balances = []
-    info = binance.get_account()
-    assets = info['balances']
-    for asset in assets:
-        if float(asset['free']) > 0.001:
-            balances.append((asset['asset'], asset['free']))
-    return(balances)
+commands = ['start', 'echo', 'gemini']
 
 
 # Telegram commands
 def start(bot, update):
-    update.message.reply_text("Hello! I am Alfred, your customized personal butler.")
-
-
-def grades(bot, update):
-    update.message.reply_text(genesis_get_grades())
-
-
-def balance(bot, update):
-    update.message.reply_text(binance_get_balance())
+    update.message.reply_text(
+        "Commands are the following: %s", commands)
 
 
 def echo(bot, update):
@@ -98,22 +26,20 @@ def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
+def gemini_balance(bot, update):
+    update.message.reply_text(gemini.get_balance())
+
 def main():
-
-    updater = Updater(config['telegram']['token'])
-
+    updater = Updater(config.telegram['token'])
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("grades", grades))
-    dp.add_handler(CommandHandler("balance", balance))
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('echo', echo))
+    dp.add_handler(CommandHandler('balance', gemini_balance()))
 
     dp.add_error_handler(error)
-
     dp.add_handler(MessageHandler(Filters.text, echo))
-
     updater.start_polling()
-
     updater.idle()
 
 
---
+++ alfred/gemini.py
@@ -0,0 +1,41 @@
+import requests
+import base64
+import hmac
+import json
+import hashlib
+import time
+import config
+
+url = "https://api.sandbox.gemini.com"
+gemini_api_key = config.geminiTestNet['apiKey']
+gemini_api_secret = config.geminiTestNet['apiSecret'].encode('utf-8')
+
+def get_balance():
+    nonce = int(time.time() * 1000)
+    payload = {
+        "request": "/v1/balances",
+        "nonce": nonce
+    }
+    b64_payload = base64.b64encode(
+        json.dumps(payload).encode('utf-8'))
+    signature = hmac.new(
+        gemini_api_secret,
+        b64_payload,
+        hashlib.sha384
+    ).hexdigest()
+    headers = {
+        'Content-Type': "text/plain",
+        'Content-Length': "0",
+        'X-GEMINI-APIKEY': gemini_api_key,
+        'X-GEMINI-PAYLOAD': b64_payload.decode('utf-8'),
+        'X-GEMINI-SIGNATURE': signature,
+        'Cache-Control': "no-cache"
+    }
+
+    r = requests.post(
+        url+"/v1/balances",
+        headers=headers)
+
+    for balance in r.json():
+        if balance['currency'] == "USD":
+            return balance['currency'] + ":" + balance['available']
