2d8cf861c2a195e39d3f41f66c378909eef8b791
Pepi <pepimartinez@protonmail.com>
2019-03-28 01:45:48 -0400

    added TODO's to config

---
+++ alfred2/core.py
@@ -0,0 +1,7 @@
+# TODO extensions need names, menus, and commands
+# TODO create default menues/commands like starting bot, restart, etc
+# TODO utils file to help create menues, etc.
+# TODO in core just check config and import all modules and create handlers/pass on api keys
+
+# TODO also need testing/logging/error handleing
+# TODO deployment
