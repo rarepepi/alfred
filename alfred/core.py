606ae57c148c69a2518c5039f4c50550a95ed30c
Pepi <pepimartinez@protonmail.com>
2019-04-05 02:44:12 -0400

    Alfred 2.0 3 month break revision/release

--- alfred/core.py
+++ alfred/core.py
@@ -1,28 +1,113 @@
-import importlib
+# TODO extensions need names, menus, and commands
+# TODO create default menues/commands like starting bot, restart, etc
+# TODO utils file to help create menues, etc.
+# TODO in core just check config and import all modules and create handlers/pass on api keys
+
+# TODO also need testing/logging/error handleing
+# TODO deployment
+import logging
 import config
+import modules
+import utils
+import importlib
+import os
+import sys
+from threading import Thread
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+from telegram.ext import (
+    CommandHandler,
+    Updater,
+    CallbackQueryHandler
+)
 
-extension_name = 0
-extension_status = 1
-objects = []
-
-for extension in config.extensions:
-    if extension[extension_status] == "active":
-        print(extension[extension_name])
-        objects.append(importlib.import_module(extension[extension_name]))
-
-for obj in objects:
-    print(object)
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def get_response(query):
-    if query == "gemini_btc_price":
-        return extensions.Gemini.get_btc_price()
+class Alfred(object):
+    def __init__(self):
+        self.updater = Updater(config.telegram['token'])
+        self.dp = self.updater.dispatcher
+        self.modules = self.import_active_extensions()
+        self.add_default_handlers()
+        self.add_module_handlers()
 
-    elif query == "gemini_btc_balance":
-        return extensions.Gemini.get_btc_balance()
+    def import_active_extensions(self):
+        extensions = config.extensions
+        modules = []
+        for dict in extensions:
+            if dict['active']:
+                module = importlib.import_module('modules.{}.core'.format(dict['name']).lower(), '.')
+                modules.append(module.Module())
+        return modules
 
-    elif query == "binance_total_balances":
-        return extensions.Binance.get_balance()
+    def add_default_handlers(self):
+        self.dp.add_handler(CommandHandler('start', self.start))
+        self.dp.add_handler(CallbackQueryHandler(self.main_menu, pattern='main'))
+        self.dp.add_handler(CommandHandler('restart', self.restart))
+        self.dp.add_error_handler(self.error)
 
-    elif query == "goldmoney_total_balances":
-        return extensions.Goldmoney.get_balance()
+    def add_module_handlers(self):
+        for module in self.modules:
+            logger.info("Adding module handler: {}".format(module.name))
+            self.dp.add_handler(
+                CallbackQueryHandler(
+                    module.main_menu, pattern='{}-main'.format(module.name)))
+            self.dp.add_handler(
+                CallbackQueryHandler(
+                    module.callback_handler))
+
+    def main_menu(self, bot, update):
+        query = update.callback_query
+        logger.info("query: {}".format(query))
+        bot.edit_message_text(chat_id=query.message.chat_id,
+            message_id=query.message.message_id,
+            text="Commands",
+            reply_markup=self.main_menu_keyboard())
+    
+    def main_menu_keyboard(self):
+        keyboard = utils.get_extension_keyboards()
+        return InlineKeyboardMarkup(keyboard)
+
+    def start(self, bot, update):
+        update.message.reply_text("Modules",
+            reply_markup=self.main_menu_keyboard())
+
+    def stop_and_restart(self):
+        self.updater.stop()
+        os.execl(
+            sys.executable,
+            sys.executable,
+            * sys.argv
+        )
+
+    def restart(self, bot, update):
+        update.message.reply_text('🖥 restarting system...')
+        Thread(target=self.stop_and_restart).start()
+        update.message.reply_text('🖥 system back online!')
+
+    def error(self, bot, update, error):
+        logger.warning(
+            'Update "%s" caused error "%s"',
+            update,
+            error
+        )
+
+    def run(self):
+        logging.info("Starting bot polling")
+        self.updater.start_polling()
+        self.updater.idle()
+
+
+def main():
+    al = Alfred()
+    al.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ alfred/modules/gemini/core.py
@@ -0,0 +1,114 @@
+import logging
+from telegram import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class Module():
+    def __init__(self):
+        self.name = "gemini"
+        self.commands = ['balance']
+
+    def resolve_query(self, query):
+        if query == "gemini-balance":
+            return self.balance()
+
+    def main_menu(self, bot, update):
+        query = update.callback_query
+        bot.edit_message_text(chat_id=query.message.chat_id,
+            message_id=query.message.message_id,
+            text="Commands",
+            reply_markup=self.module_menu_keyboard())
+
+    def get_commands_keyboard(self):
+        keyboard = []
+        for command in self.commands:
+            keyboard.append(
+                [InlineKeyboardButton(
+                    '{}'.format(command),
+                    callback_data='{}-{}'.format(
+                    self.name, command))]
+                )
+        return keyboard
+
+    def module_menu_keyboard(self):
+        keyboard = self.get_commands_keyboard()
+        return InlineKeyboardMarkup(keyboard)
+
+    def callback_handler(self, bot, update):
+        query = update.callback_query.data
+        text = self.resolve_query(query)
+        bot.send_message(
+            text=text,
+            chat_id=update.callback_query.message.chat.id
+        )
+
+    def balance(self):
+        print("hello world3")
+        return 100
+
+
+
+
+
+
+# class Gemini():
+#         def get_menu():
+#                 pass
+
+#         def get_commands():
+#                 pass
+
+#         def resolve_query():
+#                 pass
+
+#         url = "https://api.sandbox.gemini.com"
+#         gemini_api_key = config.geminiTestNet['apiKey']
+#         gemini_api_secret = config.geminiTestNet['apiSecret'].encode('utf-8')
+
+#         def get_price(symbol):
+#             r = requests.get(url+"/v1/pubticker/"+symbol)
+#             price = r.json()
+#             return price['last']
+
+#         def get_btc_price():
+#             return get_price("btcusd")
+
+#         def get_btc_balance():
+#             return get_balance("BTC")
+
+#         def get_balance(symbol):
+#             nonce = int(time.time() * 1000)
+#             payload = {
+#                 "request": "/v1/balances",
+#                 "nonce": nonce
+#             }
+#             b64_payload = base64.b64encode(
+#                 json.dumps(payload).encode('utf-8'))
+#             signature = hmac.new(
+#                 gemini_api_secret,
+#                 b64_payload,
+#                 hashlib.sha384
+#             ).hexdigest()
+#             headers = {
+#                 'Content-Type': "text/plain",
+#                 'Content-Length': "0",
+#                 'X-GEMINI-APIKEY': gemini_api_key,
+#                 'X-GEMINI-PAYLOAD': b64_payload.decode('utf-8'),
+#                 'X-GEMINI-SIGNATURE': signature,
+#                 'Cache-Control': "no-cache"
+#             }
+
+#             r = requests.post(
+#                 url+"/v1/balances",
+#                 headers=headers)
+
+#             for balance in r.json():
+#                 if balance['currency'] == symbol:
+#                     return balance['currency'] + ":" + balance['available']
