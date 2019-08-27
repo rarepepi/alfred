87ebc3ba0102b5043f9675bcb1854e0d39222a2a
Pepi <pepimartinez@protonmail.com>
2019-08-27 13:49:15 -0400

    updated sample module

--- docs/sample.module.py
+++ docs/sample.module.py
@@ -4,45 +4,14 @@ from telegram import (
 )
 
 
-class ModuleBasic():
+
+class Module(AlfredModule):
     def __init__(self):
-        self.name = "name"
-        self.commands = ['command1', 'command2']
+        self.name = "mod_name"
+        self.menu_name = "📟 Pretty Mod Name"
+        self.commands = [
+            ('💰 Balance', "get_balance_detailed"),
+        ]
 
-    def resolve_query(self, query):
-        if query == "name-command":
-            return self.command()
-
-    def main_menu(self, bot, update):
-        query = update.callback_query
-        bot.edit_message_text(
-            chat_id=query.message.chat_id,
-            message_id=query.message.message_id,
-            text="Commands",
-            reply_markup=self.module_menu_keyboard())
-
-    def get_commands_keyboard(self):
-        keyboard = []
-        for command in self.commands:
-            keyboard.append(
-                [InlineKeyboardButton(
-                    '{}'.format(command),
-                    callback_data=f'{self.name}-{command}')]
-                )
-        return keyboard
-
-    def module_menu_keyboard(self):
-        keyboard = self.get_commands_keyboard()
-        return InlineKeyboardMarkup(keyboard)
-
-    def callback_handler(self, bot, update):
-        query = update.callback_query.data
-        text = self.resolve_query(query)
-        bot.send_message(
-            text=text,
-            chat_id=update.callback_query.message.chat.id
-        )
-
-    def command(self):
-        print("hello world")
-        return 100
+    def get_balance_detailed(self):
+        return "Anything you want"
