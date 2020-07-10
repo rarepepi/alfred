822d1a645ee6e38b8b45a2582763a1f6ec7665a4
Pepi <pepimartinez@protonmail.com>
2020-07-10 16:53:43 -0400

    sample config for how to use apis of exchanges

---
+++ alfred/modules/configs/sample_config.py
@@ -0,0 +1,26 @@
+telegram = {
+    "chat_id": "",
+    "username": ""
+}
+
+gemini = {
+    "api_key": "",
+    "api_secret": ""
+}
+
+goldmoney = {
+    "gold": "",
+    "silver": ""
+}
+
+td_ameritrade = {
+    "account_number": "",
+    "account_password": "",
+    "consumer_id": "",
+    "redirect_uri": ""
+}
+
+binance = {
+    "api_key": "",
+    "secret_key": ""
+}
